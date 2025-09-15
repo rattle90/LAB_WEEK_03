14311ce3df34891edb955e5e693bf5649e0bb1ca
rattle90 <yuapedroza@gmail.com>
2025-09-15 23:09:03 +0700

    Commit No. 2 – add coffee list & detail fragment

--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -7,7 +7,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+interface CoffeeListener {
+    fun onSelected(id: Int)
+}
+class MainActivity : AppCompatActivity(), CoffeeListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,29 +22,11 @@ class MainActivity : AppCompatActivity() {
                 systemBars.bottom)
             insets
         }
-        Log.d(TAG, "onCreate")
     }
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart")
-    }
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume")
-    }
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
-    companion object {
-        private const val TAG = "MainActivity"
+    override fun onSelected(id: Int){
+        val detailFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment_detail)
+                as DetailFragment
+        detailFragment.setCoffeeData(id)
     }
 }
